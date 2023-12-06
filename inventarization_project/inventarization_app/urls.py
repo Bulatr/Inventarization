fc2fe02e99045249a6b6b10a9bc6ddd6314d40bc
Bulatr <bulat_r@inbox.ru>
2023-12-06 22:19:18 +0800

    Наладил нормальную работу формы, без сохр

--- inventarization_project/inventarization_app/urls.py
+++ inventarization_project/inventarization_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import index, edit_item, search, scan_qr, import_data
+from .views import index, edit_item, search, scan_qr
 
 urlpatterns = [
     path('', index, name='index'),
     path('edit_item/<int:item_id>/', edit_item, name='edit_item'),
     path('search/', search, name='search'),
     path('scan_qr/', scan_qr, name='scan_qr'),
-    path('import_page/', import_data, name='import_data'),
 ]
--- inventarization_project/inventarization_app/views.py
+++ inventarization_project/inventarization_app/views.py
@@ -2,55 +2,11 @@ from django.shortcuts import render
 from .utils import update_google_sheets, generate_qr_code
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import InventoryItem
-from .forms import InventoryItemForm, ImportForm
+from .forms import InventoryItemForm
 import gspread
 import os
 from oauth2client.service_account import ServiceAccountCredentials
 
-def import_data(request):
-    if request.method == 'POST':
-        form = ImportForm(request.POST)
-        if form.is_valid() and form.cleaned_data['import_button']:
-            # Подключение к Google Sheets
-            current_dir = os.path.dirname(os.path.abspath(__file__))
-            json_keyfile = os.path.join(current_dir, 'google', 'inventarization-406909-2d4115d42a80.json')
-            spreadsheet_name = '1954jT48OlyuveDW4qW21796c_1AurAvlN-eYVKm6Zys'
-            sheet_name = 'Наш список 2023'
-            scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-            credentials = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
-            gc = gspread.authorize(credentials)
-            worksheet = gc.open_by_key(spreadsheet_name).worksheet(sheet_name)
-            # Получаем все значения из листа
-            all_rows = worksheet.get_all_values()
-
-            # Первая строка содержит заголовки колонок
-            headers = all_rows[0]
-
-            # Остальные строки содержат данные
-            data = all_rows[1:]
-
-            # Импорт данных в модель InventoryItem
-            for row_data in data:
-                inventory_item_data = dict(zip(headers, row_data))
-                InventoryItem.objects.create(
-                    number=inventory_item_data['Номер'],
-                    inventory_number=inventory_item_data['Инвентарный номер'],
-                    new_number=inventory_item_data['Новый номер'],
-                    previous_year_number=inventory_item_data['Присвоенный номер в прошлом году'],
-                    match_with_accounting=inventory_item_data['Совпадение с бухгалтерией'],  # Пример условия
-                    location=inventory_item_data['Местоположение'],
-                    equipment_type=inventory_item_data['Тип оборудования'],
-                    model_if_not_matching=inventory_item_data.get('Модель если не совпадает со списком', ''),
-                )
-
-            return redirect('import_success')
-    else:
-        form = ImportForm()
-
-    return render(request, 'inventarization_app/import_page.html', {'form': form})
-
-def import_success(request):
-    return render(request, 'inventarization_app/import_success.html')
 
 def index(request):
     # Подключение к Google Sheets
@@ -102,7 +58,7 @@ def edit_item(request, item_id):
 
     # Укажите имена листов и заголовки столбцов
     sheet_name = 'Наш список 2023'
-    columns = ['Номер','Инвентарный номер', 'Новый номер', 'Совпадение с бухгалтерией', 'Местоположение', 'Тип оборудования', 'Модель если не совпадает со списком']
+    columns = ['Номер','Инвентарный номер', 'Новый номер', 'Присвоенный номер в прошлом году', 'Совпадение с бухгалтерией', 'Местоположение', 'Тип оборудования', 'Модель если не совпадает со списком']
 
     # Авторизация в Google Sheets API
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -113,24 +69,30 @@ def edit_item(request, item_id):
         # Открываем таблицу
         worksheet = gc.open_by_key(spreadsheet_name).worksheet(sheet_name)
         # Получаем данные по индексу строки
-        row_data = worksheet.row_values(int(item_id)+1)
+        # Индекс строки начинается с 0, поэтому нужно добавить 1
+        index_gt = int(item_id)+1
+        row_data = worksheet.row_values(index_gt)
         # Проверяем, что строка существует
         if not row_data:
             raise Http404("Строка не найдена")
 
         # Заполняем форму данными из строки
+        if len(row_data) >= 9:
+            model_if_not_matching = row_data[8]
+        else:
+            model_if_not_matching = ""
         form_data = {
-            'number': row_data[0],
-            'inventory_number': row_data[1],
-            'new_number': row_data[2],
-            'previous_year_number': row_data[3],
-            'match_with_accounting': row_data[5],
-            'location': row_data[6],
-            'equipment_type': row_data[7],
-            'model_if_not_matching': row_data[4]
+                'number': row_data[0],
+                'inventory_number': row_data[1],
+                'new_number': row_data[3],
+                'previous_year_number': row_data[4],
+                'match_with_accounting': row_data[5],
+                'location': row_data[6],
+                'equipment_type': row_data[7],
+                'model_if_not_matching': model_if_not_matching,
+                # Добавьте другие поля по необходимости
+            }
 
-            # Добавьте другие поля по необходимости
-        }
 
         if request.method == 'POST':
             # Если форма отправлена, обрабатываем данные

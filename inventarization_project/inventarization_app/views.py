e0df0327ad8c1b70a3989ec0233002a483d639f5
Bulatr <bulat_r@inbox.ru>
2023-12-10 17:23:08 +0800

    Нормально работает сохранение

--- inventarization_project/inventarization_app/views.py
+++ inventarization_project/inventarization_app/views.py
@@ -73,11 +73,6 @@ def edit_item(request, item_id):
         index_gt = int(item_id)+1
         row_data = worksheet.row_values(index_gt)
 
-        # Получаем формулу из ячейки, чтобы не переписать их при сохранении
-        formula_value_previous_year_number = worksheet.cell(index_gt, 4).value
-        formula_value_match_with_accounting = worksheet.cell(index_gt, 5).value
-        formula_value_hidden_inventory_number = worksheet.cell(index_gt, 2).value
-
         # Проверяем, что строка существует
         if not row_data:
             raise Http404("Строка не найдена")
@@ -118,6 +113,9 @@ def edit_item(request, item_id):
             worksheet.update_cell(index_gt, 8, request.POST.get('equipment_type'))
             worksheet.update_cell(index_gt, 9, request.POST.get('model_if_not_matching'))
 
+            # Готовим form_data
+            # Заново получим эту строку из гугл таблицы
+            form_data = get_sheet_item(item_id)
             # Перенаправляем на страницу с подробностями после успешного обновления
             # ! осталось разобраться с передачей измененных данных
             return render(request, 'inventarization_app/edit_item.html', {'form_data': form_data, 'item_id': item_id})
@@ -129,7 +127,70 @@ def edit_item(request, item_id):
         print(f"Ошибка API: {e}")
         raise Http404("Ошибка API Google Sheets")
 
+# Подключение к таблице и получении данных
+# возвращает  form_data
+def get_sheet_item(item_id):
+    # Подключение к Google Sheets
+    # Получить абсолютный путь к текущему файлу (где находится views.py)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
+    # Укажите путь к файлу ключа JSON, полученному при создании сервисного аккаунта в Google Cloud Console
+    json_keyfile = os.path.join(current_dir, 'google', 'inventarization-406909-2d4115d42a80.json')
+
+    # Укажите имя вашего файла Google Spreadsheet
+    spreadsheet_name = '1954jT48OlyuveDW4qW21796c_1AurAvlN-eYVKm6Zys'
+
+    # Укажите имена листов и заголовки столбцов
+    sheet_name = 'Наш список 2023'
+
+    # Авторизация в Google Sheets API
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
+    gc = gspread.authorize(credentials)
+
+    try:
+        # Открываем таблицу по имени
+        # Открываем таблицу
+        worksheet = gc.open_by_key(spreadsheet_name).worksheet(sheet_name)
+        # Получаем данные по индексу строки
+        # Индекс строки начинается с 0, поэтому нужно добавить 1
+        index_gt = int(item_id)+1
+        row_data = worksheet.row_values(index_gt)
+
+        # Проверяем, что строка существует
+        if not row_data:
+            raise Http404("Строка не найдена")
+        # проверка на существование колонок, иногда ведет неадекватно, не может прочитать пустые значения
+        if len(row_data) >=7:
+            lenrow_data = len(row_data)
+            location = row_data[6]
+            equipment_type = row_data[7]
+        else:
+            location = ""
+            equipment_type = ""
+        # Заполняем форму данными из строки
+        if len(row_data) >= 9:
+            model_if_not_matching = row_data[8]
+        else:
+            model_if_not_matching = ""
+
+        form_data = {
+                'number': row_data[0],
+                'inventory_number': row_data[1],
+                'new_number': row_data[3],
+                'previous_year_number': row_data[4],
+                'match_with_accounting': row_data[5],
+                'location': location,
+                'equipment_type': equipment_type,
+                'model_if_not_matching': model_if_not_matching,
+                # Добавьте другие поля по необходимости
+            }
+        return form_data
+
+    except gspread.exceptions.APIError as e:
+        # Обработка ошибок, связанных с API Google Sheets
+        print(f"Ошибка API: {e}")
+        raise Http404("Ошибка API Google Sheets")
 
 def search(request):
     if request.method == 'GET':
